cb9aa94ab19cb86cbf28cd1d5163c24f563bdbf5
majidpawar9 <majidpawar9@gmail.com>
2023-07-07 01:56:23 -0400

    Made layout and added button as well as score count

--- app/src/main/java/com/majid/scorekeepingapp/MainActivity.kt
+++ app/src/main/java/com/majid/scorekeepingapp/MainActivity.kt
@@ -2,8 +2,11 @@ package com.majid.scorekeepingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var team1Button:Button
+    private lateinit var team2Button:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
