3625dbeda20e0e94642737a8730f786d06cb16e6
majidpawar9 <majidpawar9@gmail.com>
2023-07-07 03:45:07 -0400

    Added comments and did slight changes in strings.xml

--- app/src/main/java/com/majid/scorekeepingapp/MainActivity.kt
+++ app/src/main/java/com/majid/scorekeepingapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    //used lateinit to keep to property from being initialized
     private lateinit var team1Button1Pointer:Button
     private lateinit var team2Button1Pointer:Button
     private lateinit var team1Button2Pointer:Button
@@ -19,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //finding views by ID and storing them in Button object
         team1Button1Pointer = findViewById(R.id.teamone1pointer)
         team2Button1Pointer = findViewById(R.id.teamtwo1pointer)
         team1Button2Pointer = findViewById(R.id.teamone2pointer)
@@ -28,9 +30,12 @@ class MainActivity : AppCompatActivity() {
         reset = findViewById(R.id.resetButton)
         team1Score = findViewById(R.id.team1Score)
         team2Score = findViewById(R.id.team2Score)
+
+        //define score count for each time
         var team1Scored = 0
         var team2Scored = 0
 
+        //onclick events
         team1Button1Pointer.setOnClickListener{
 
             team1Scored +=1
@@ -58,6 +63,8 @@ class MainActivity : AppCompatActivity() {
             team2Scored +=3
             team2Score.text = team2Scored.toString()
         }
+
+        //will reset the game
         reset.setOnClickListener{
             team1Scored = 0
             team2Scored = 0
